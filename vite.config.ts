3b86fa59186dd4d47c836d423520d72f3b49e03e
EdenLustosa <edenlustosa@hotmail.com>
2026-04-15 10:56:06 -0300

    Add path aliasing and public directory to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: "client/public",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client/src"),
+      "@shared": path.resolve(__dirname, "./shared"),
+    },
+  },
 });
